d2154b4b3bc374494a0e09d9c302e6f3193c98aa
Arseny Konohov <arsenykonohov@gmail.com>
2021-03-21 18:47:21 +0300

    basic js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,39 @@
 // SPECIFY TARGET DOM EVENT HANDLERS:
 const list = [...document.querySelectorAll('.piano__key')];
+
+// key listeners
 document.addEventListener('keydown', pianoKeydown);
 document.addEventListener('keyup', pianoKeyup);
-list.forEach(node => node.addEventListener('mousedown', pianoMousedown));
-list.forEach(node => node.addEventListener('mouseup', pianoMouseup));
-list.forEach(node => node.addEventListener('mouseover', pianoMouseover));
+
+// mouse listeners
+list.forEach(node => node.addEventListener('mousedown',  pianoMousedown));
+list.forEach(node => node.addEventListener('mouseup',    pianoMouseup));
+list.forEach(node => node.addEventListener('mouseover',  pianoMouseover));
+list.forEach(node => node.addEventListener('mouseleave', pianoMouseleave));
+
+
 
 
 
 // IMPLEMENTATION:
-// key
-function pianoKeydown(e) {}
-function pianoKeyup(e) {}
+// key manipulation
+function pianoKeydown(e) {
+    console.log('KEY DOWN:', e.code);
+}
+function pianoKeyup(e) {
+    console.log('KEY UP:', e.code);
+}
 
-// mouse
-function pianoMousedown(e) {}
-function pianoMouseup(e) {}
-function pianoMouseover(e) {}
+// mouse manipulation
+function pianoMousedown(e) {
+    console.log('MOUSE DOWN:', e.target.dataset.code);
+}
+function pianoMouseup(e) {
+    console.log('MOUSE UP:', e.target.dataset.code);
+}
+function pianoMouseover(e) {
+    console.log('MOUSE OVER:', e.target.dataset.code);
+}
+function pianoMouseleave(e) {
+    console.log('MOUSE LEAVE:', e.target.dataset.code);
+}
